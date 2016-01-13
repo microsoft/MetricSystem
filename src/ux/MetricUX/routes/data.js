7ed4c36a1b2aa80d6cbd4208dc7d1b4df835ec69
Jordan Edwards <jordane@outlook.com>
2016-01-13 11:08:34 -0800

    removed unnecessary references

--- src/ux/MetricUX/routes/data.js
+++ src/ux/MetricUX/routes/data.js
@@ -5,18 +5,18 @@ var router = express.Router();
 
 /* GET environment list */
 router.get("/environments", function (req, res) {
-    request.get('http://xaptools.bn1.binginternal.com/MetricUX/Data/GetEnvironments',
+    request.get('https://proxy/MetricUX/Data/GetEnvironments',
         function (error, response, body) { res.json(["xap-prod-co3b","xap-prod-bn1"]) });//JSON.parse(body)) });
 });
 
 /* GET machines in an environment*/
 router.get("/machines", function (req, res) {
-    request.get('http://xaptools.bn1.binginternal.com/MetricUX/Data/UpdateMachineList?environment='+req.query.environment,
+    request.get('https://proxy/MetricUX/Data/UpdateMachineList?environment='+req.query.environment,
         function (error, response, body) { res.json(JSON.parse(body)) });
 });
 
 router.post("/info", function (req, res) {
-    request.post('http://xaptools.bn1.binginternal.com/MetricUX/Data/GetJSON',
+    request.post('https://proxy/MetricUX/Data/GetJSON',
     { form: { machineName: req.body.machineName, environmentName: req.body.environmentName, counterName: req.body.counterName, queryCommand: req.body.queryCommand, queryParameters: req.body.queryParameters, timeoutValue: 2500  } },
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -40,7 +40,7 @@ router.post("/info", function (req, res) {
 });
 
 router.post("/query", function (req, res) {
-    request.post('http://xaptools.bn1.binginternal.com/MetricUX/Data/GetJSON',
+    request.post('https://proxy/MetricUX/Data/GetJSON',
     { form: { queryCommand: "query", counterName: req.body.counterName, queryParameters: req.body.queryParameters, environmentName: req.body.environmentName, timeoutValue: req.body.timeoutValue } },
     function (error, response, body) {
         if (!error && response.statusCode === 200) {
